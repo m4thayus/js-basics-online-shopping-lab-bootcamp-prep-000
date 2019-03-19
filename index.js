fc5ea289558ae1b5d61c5b3ee533a8f6604cb3d1
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 20:38:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,6 +50,6 @@ function placeOrder(cardNumber) {
   }
   else {
     cart = [];
-    return 'Your total cost is ' + total() + `, which will be charged to the card ${cardNumber}`;
+    return 'Your total cost is $' + total() + `, which will be charged to the card ${cardNumber}`;
   }
 }
