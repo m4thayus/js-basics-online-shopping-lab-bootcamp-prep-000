9d878cb5078beda8bdb64b1be3cbbf30479abd78
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 20:13:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  var newItem = {};
- n = Math.floor(Math.random() * Math.floor(max));
+ n = Math.floor(Math.random() * 100) + 1;
  newItem = {'name': item, 'price': n}
  cart.push(newItem);
  return `${item} has been added to your cart.`;
